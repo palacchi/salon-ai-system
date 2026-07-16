5a57c027227721e3c7bd8d4b77d0cdb845be340a
palacchi <poteti1981@hotmail.co.jp>
2026-07-16 09:41:35 +0900

    サーバーレス関数にplaywright-coreの必要ファイルを含めるよう設定

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/api/*": ["./node_modules/playwright-core/**"],
+  },
 };
 
 export default nextConfig;
